38fcf8d0c59091c6aa7e11be844aec0ce66e1110
Fikayomi31 <fikayoogundijo@gmail.com>
2024-01-13 00:47:07 -0800

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""lockbox"""
+
+
+def canUnlockAll(boxes):
+    """method that determines if all boxes can be opened
+    
+    Arg:
+        boxes -- a list of lists
+    Return: True if all boxes can be opened else false
+    """
+    
