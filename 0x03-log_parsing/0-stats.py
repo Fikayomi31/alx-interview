7cb8ef4c5678d883480ef1e4e9677a0f5fee13c4
Fikayomi31 <fikayoogundijo@gmail.com>
2024-03-21 15:31:45 -0700

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,7 +25,7 @@ import sys
 status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                '404': 0, '405': 0, '500': 0}
 total_size = 0
-count = 0 #  keep count of numbers of line counted
+count = 0  # keep count of numbers of line counted
 
 try:
     for line in sys.stdin:
@@ -44,7 +44,7 @@ try:
             count += 1
 
         if count == 10:
-            count = 0 #  reset count
+            count = 0  # reset count
             print('File size: {}'.format(total_size))
 
             """Print out the status code counts"""
